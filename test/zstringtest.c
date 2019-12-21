9ad57fb2e7d6a0f56a1a7f0ad7e71f4376250d6b
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2019-12-22 12:55:33 +1300

    updated gitignore

---
+++ test/zstringtest.c
@@ -0,0 +1,47 @@
+/******************************************************************************
+* zstringtest.c
+* Copyright (c) 2019, Fehmi Noyan ISI fnoyanisi@yahoo.com
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*   notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimer in the
+*   documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY Fehmi Noyan ISI ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Fehmi Noyan ISI BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include <stdio.h>
+#include <unity.h>
+
+void setUp(void){
+    printf("setup\n");
+}
+
+void tearDown(void){
+    printf("teardown\n");
+}
+
+void test_nothing(void){
+    TEST_ASSERT_EQUAL_INT(12, 12);
+}
+
+int main(){
+    UNITY_BEGIN();
+        RUN_TEST(test_nothing);
+    return UNITY_END();
+}
+
